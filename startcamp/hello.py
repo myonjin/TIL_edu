0ce2cdf2fff84f4136e180a4c3ca025a17f0dc83
myonjin <kjinabc@gmail.com>
2022-07-15 14:39:52 +0900

    SC Day2 | python intro

---
+++ startcamp/hello.py
@@ -0,0 +1,34 @@
+
+greeting = '반갑습니다.'
+
+
+
+player = 5   
+
+
+for _ in range(player):
+    print('hi')
+
+
+def bot():
+    player = 5 
+    while player > 0:
+        print(greeting)
+        player = player - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
