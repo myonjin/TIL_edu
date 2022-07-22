7ce238a482df5e160d93e96ec28b0bbb9b62ccbb
myonjin <kjinabc@gmail.com>
2022-07-23 00:33:26 +0900

    ss_pjt

--- WorkShop/0721/dsd.py
+++ WorkShop/0721/dsd.py
@@ -1,11 +1,9 @@
+a=[[1],[2,3],[4,5,6],[7,8,9,10]]
+result=0
+for i in a:
+    for j in a[i]:
+        result+=a[i][j]
 
-def all_list_sum(scores):          
-    for i in range(len(scores)):  # scores의 길이는 8이다.
-        sum = 0
-    
-        for j in range(len(scores[i])):  # 2중리스트의 각 요소에 접근하기 위해
-            sum = sum + scores[i][j]
-    return sum
+print(result)
 
-print(all_list_sum([[1],[2,3],[4,5,6],[7,8,9,10]]))
 
